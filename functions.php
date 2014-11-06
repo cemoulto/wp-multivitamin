76af4d74a8b2fa34c59086147d8d5827818af37b
Stefan Erickson <sc.erickson@icloud.com>
2014-11-06 14:24:46 -0600

    Glob post type separately

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
-foreach (glob("config/**/*.php") as $config_item) {
+foreach (glob("config/*.php") as $config_item) {
   include $config_item;
+}
+
+foreach (glob("config/post_types/*.php") as $post_type) {
+  include $post_type;
 }
 
 ?>
